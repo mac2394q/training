e63b89b8353eb2da1e6ef8618299d0715f3e27d9
DESKTOP-MCPLJ80\senio <mac2394q@gmail.com>
2021-06-28 18:29:24 -0500

    init project - mongo repositories

--- boot_mongodb_api/demo/src/main/java/com/example/demo/DemoApplication.java
+++ boot_mongodb_api/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,16 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +18,44 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner runner(
+			StudentRepository repository,
+			MongoTemplate mongoTemplate
+	){
+		return args -> {
+			address adress = new address( "colombia",
+					"tulua",
+					"057000");
+
+			String email = "mac2394q@gmail.com";
+			Student student = new Student(
+					"miguel",
+					"claros",
+					"mac2394q@gmail.com",
+					Gender.MALE,
+					adress,
+					List.of("senior enginer"),
+					BigDecimal.TEN,
+					LocalDateTime.now()
+			);
+
+			Query query = new Query();
+			query.addCriteria( Criteria.where( "email" ).is( email )  );
+
+			List< Student > students = mongoTemplate.find( query, Student.class );
+			if(  students.size() > 1 ){
+				throw new IllegalStateException(" found many students with email "+email);
+			}
+
+			if( students.isEmpty() ){
+				repository.insert( student );
+			}
+	
+
+		};
+	}
+
 
 }
---
+++ boot_mongodb_api/demo/src/main/java/com/example/demo/address.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class address {
+
+    private String country;
+    private String city;
+    private String postCode;
+
+
+}
